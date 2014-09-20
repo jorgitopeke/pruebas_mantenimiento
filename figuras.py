48a26721e881db3a767a713dccdfb8682c19b432
jorgitopeke <jorgito.c.2011@gmail.com>
2014-09-19 19:02:38 -0500

    cambio en el codigo
    km

--- figuras.py
+++ figuras.py
@@ -1,15 +1,15 @@
 class figuras():
     def areaCirculo(self, radio):
-        return 3.1416 * (num1 * num1)
+        return 3.1416 * (radio * radio)
 	
     def areaCuadrado(self, lado):
-        return num1 * num1
+        return lado * lado
 
     def areaTriangulo(self, base,altura):
         return (base * altura)/2
     
     def areaRectangulo(self, base, altura):
-        return basde * altura
+        return base * altura
 
     def areaRombo(self, dmayor, dmenor):
         return (dmayor * dmenor)/2
