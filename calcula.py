8cb95bf8d5324e63dda25dc257f5dcded3a58dd1
jorgitopeke <jorgito.c.2011@gmail.com>
2014-09-19 19:17:06 -0500

    calculadora
    ks

---
+++ calcula.py
@@ -0,0 +1,16 @@
+class Calculator():
+
+	def suma(self, num1, num2):
+        	return num1 + num2
+
+	def resta(self, num1, num2):
+        	return num1 - num2
+
+	def mult(self, num1, num2):
+        	return num1 * num2
+
+	def div(self, num1, num2):
+        	return num1 / num2
+
+	
+	
