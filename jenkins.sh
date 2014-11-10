a69da66c0ab365b8aaa7a6e2afb546bd3e82753c
jorgitopeke <jorgito.c.2011@gmail.com>
2014-11-10 08:51:34 -0600

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+pip install -r requirements.txt
+autopep8 -ir *.py
+flake8 --max-complexity=3 --exclude=*.txt,*.md --max-line-length=200 *.py
+cd test
+lettuce 
+cd ..
+python TestFiguras.py -v
+coverage run --branch TestFiguras.py
+coverage report -m
+coverage html
