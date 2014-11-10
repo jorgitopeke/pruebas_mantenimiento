61649e21b68c609a212472d9d8618564b65ae9e3
jorgitopeke <jorgito.c.2011@gmail.com>
2014-11-10 09:28:02 -0600

    Update TestFiguras.py

--- TestFiguras.py
+++ TestFiguras.py
@@ -6,11 +6,11 @@ class TestFiguras(unittest.TestCase):
         f = figuras()
         self.assertEqual(12.5664,f.areaCirculo(2))
 
-    def test_Area_Circulo(self):
+    def test_Area_Circulo1(self):
         f = figuras()
         self.assertEqual(78.54, f.areaCirculo(5))
 
-    def test_Area_Circulo(self):
+    def test_Area_Circulo2(self):
         f = figuras()
         self.assertEqual(380.1336, f.areaCirculo(11))
 	
@@ -19,11 +19,11 @@ class TestFiguras(unittest.TestCase):
         f = figuras()
         self.assertEqual(3, f.areaCuadrado(2))
 	
-    def test_Area_Cuadrado(self):
+    def test_Area_Cuadrado2(self):
         f = figuras()
         self.assertEqual(100, f.areaCuadrado(10))
 
-    def test_Area_Cuadrado(self):
+    def test_Area_Cuadrado3(self):
         f = figuras()
         self.assertEqual(25, f.areaCuadrado(5))
 
@@ -31,11 +31,11 @@ class TestFiguras(unittest.TestCase):
         f = figuras()
         self.assertEqual(2, f.areaTriangulo(2,2))
 	
-    def test_Area_Triangulo(self):
+    def test_Area_Triangulo1(self):
         f = figuras()
         self.assertEqual(6, f.areaTriangulo(6,2))
 
-    def test_Area_Triangulo(self):
+    def test_Area_Triangulo2(self):
         f = figuras()
         self.assertEqual(25, f.areaTriangulo(10,5))
 
@@ -43,11 +43,11 @@ class TestFiguras(unittest.TestCase):
         f = figuras()
         self.assertEqual(4, f.areaRectangulo(2,2))
 
-    def test_Area_Rectangulo(self):
+    def test_Area_Rectangulo2(self):
         f = figuras()
         self.assertEqual(100, f.areaRectangulo(2,50))
 
-    def test_Area_Rectangulo(self):
+    def test_Area_Rectangulo3(self):
         f = figuras()
         self.assertEqual(80, f.areaRectangulo(80,1))
 	
@@ -55,11 +55,11 @@ class TestFiguras(unittest.TestCase):
         f = figuras()
         self.assertEqual(2, f.areaRombo(2,2))
 
-    def test_Area_Rombo(self):
+    def test_Area_Rombo2(self):
         f = figuras()
         self.assertEqual(150, f.areaRombo(100,3))
 
-    def test_Area_Rombo(self):
+    def test_Area_Rombo3(self):
         f = figuras()
         self.assertEqual(20, f.areaRombo(5,8))
 if __name__=="__main__":
